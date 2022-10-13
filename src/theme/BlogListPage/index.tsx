78db33eb87020dfa17a3374d5f0384d055eb0ea5
Vugar Dadalov <vuqar.ahmedoglu@gmail.com>
2022-10-13 13:11:58 +0400

    search fixed

--- src/theme/BlogListPage/index.tsx
+++ src/theme/BlogListPage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import type { Props } from "@theme/BlogListPage";
@@ -9,8 +9,12 @@ import { Tags } from "./tags";
 import { Header } from "./header";
 import { Cards } from "./card";
 import { Search } from "./search";
+import { NoResult } from "./no-result";
 
 export default function BlogListPage(props: Props | TagProps) {
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
+
   const metadata = "listMetadata" in props ? props.listMetadata : props.metadata;
 
   const {
@@ -23,24 +27,45 @@ export default function BlogListPage(props: Props | TagProps) {
 
   const tag = "tag" in props ? props.tag : null;
 
+  const onChange = (val: string) => {
+    setSearchInput(val);
+    if (val !== "") {
+      const filteredData = props.items.filter((item) => {
+        return [
+          item.content.frontMatter.title.toLowerCase(),
+          ...item.content.metadata.tags.map((a) => a.label.toLowerCase()),
+          ...item.content.metadata.authors.map((a) => a.name.toLowerCase()),
+          item.content.metadata.formattedDate.toLowerCase(),
+        ].some((a) => a.includes(val.toLowerCase()));
+      });
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults([...props.items]);
+    }
+  };
+
+  const List = ({ list }) => {
+    return !list.length ? (
+      <NoResult />
+    ) : (
+      <div className="mt-12 grid grid-flow-row gap-6 text-neutral-600 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+        {list.map(({ content }) => (
+          <Cards key={content.metadata.permalink} content={content as any} tag={tag} />
+        ))}
+      </div>
+    );
+  };
+
   return (
     <Layout title={title} description={blogDescription} wrapperClassName={ThemeClassNames.wrapper.blogPages}>
       <main className="max-w-7xl mx-auto mt-2 md:mt-20 mb-8 p-6">
         <Header />
         <div className="w-full flex gap-5 gap-x-24 justify-between flex-wrap-reverse">
           <Tags activeTag={metadata.permalink} />
-          <Search />
+          <Search onChange={onChange} />
         </div>
 
-        {/* <div className="mt-12">
-          <h2>{tag ? `Articles tagged with "${tag.label}"` : "Blog Posts"}</h2>
-        </div> */}
-
-        <div className="mt-12 grid grid-flow-row gap-6 text-neutral-600 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-          {props.items.map(({ content }) => (
-            <Cards key={content.metadata.permalink} content={content as any} tag={tag} />
-          ))}
-        </div>
+        {searchInput.length > 1 ? <List list={filteredResults} /> : <List list={props.items} />}
       </main>
     </Layout>
   );
--- src/theme/BlogListPage/search.tsx
+++ src/theme/BlogListPage/search.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-export const Search = function () {
+export const Search = function ({onChange}) {
   return (
     <>
       <form className="flex-grow basis-64">
@@ -15,9 +15,9 @@ export const Search = function () {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               ></path>
             </svg>
@@ -45,6 +45,7 @@ export const Search = function () {
             dark:focus:border-blue-500"
             placeholder="Search by name, title, category..."
             required
+            onChange={(e) => onChange(e.target.value)}
           />
         </div>
       </form>
