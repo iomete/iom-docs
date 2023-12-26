d2225daad8eee5e912b56e6ba0cf9782c5f25b3a
Vugar Dadalov <vuqar.ahmedoglu@gmail.com>
2023-12-26 16:26:04 +0400

    added tailwindcss

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -193,7 +193,18 @@ const config = {
           },
         ],
       },
-    ]
+    ],
+    async function myPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
   ]
 };
 
