8a9ad3e89745f7b023ca9c32f8603cef4a159a54
Vugar Dadalov <vuqar.ahmedoglu@gmail.com>
2023-12-28 01:24:03 +0400

    Blog: og image refactoring

--- src/theme/BlogTagsListPage/index.js
+++ src/theme/BlogTagsListPage/index.js
@@ -3,11 +3,15 @@ import BlogTagsListPage from '@theme-original/BlogTagsListPage';
 import Head from '@docusaurus/Head';
 
 export default function BlogTagsListPageWrapper(props) {
+  const canonicalUrl = "https://iomete.com/blog";
   return (
     <>
       <Head>
         <meta name="robots" content="noindex, nofollow, noarchive" />
         <meta name="googlebot" content="noindex, nofollow, noarchive" />
+
+        <link rel="canonical" href={canonicalUrl}></link>
+        <meta property="og:url" content={canonicalUrl} />
       </Head>
       <BlogTagsListPage {...props} />
     </>
