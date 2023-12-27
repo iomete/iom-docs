793d08e819055e0bf878876ecd66b0cae87c71c4
Vugar Dadalov <vuqar.ahmedoglu@gmail.com>
2023-12-27 15:39:42 +0400

    Blog list header and no result added

---
+++ src/theme/BlogListPage/empty/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+function Empty() {
+  return <div className={styles.Empty}>No result found</div>;
+}
+
+export default Empty;
