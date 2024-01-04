112ecfffee0551492c4d892750b264e3c8a423f9
Nurlan Mammadov <36840386+nurlanmamedov@users.noreply.github.com>
2024-01-04 12:14:54 +0100

    [ guides ] cards link add base url

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./style.scss";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
-// import { ArrowRight } from "@phosphor-icons/react";
-
-const Card = ({ title, icon, link, children }) => (
-  <div className="iom-card">
-    <Link to={link}>
-      <div className="wrapper">
-        {icon && <div className="icon-wrapper">{icon}</div>}
-        <div className="content">
-          <h3 className="title">{title}</h3>
-          {children}
+const Card = ({ title, icon, link, children }) => {
+  const baseUrl = useBaseUrl("/");
+  return (
+    <div className="iom-card">
+      <Link to={baseUrl + link}>
+        <div className="wrapper">
+          {icon && <div className="icon-wrapper">{icon}</div>}
+          <div className="content">
+            <h3 className="title">{title}</h3>
+            {children}
+          </div>
         </div>
-      </div>
-    </Link>
-  </div>
-);
+      </Link>
+    </div>
+  );
+};
 
 export default Card;
