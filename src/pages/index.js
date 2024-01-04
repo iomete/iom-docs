a9fd65492885019bffaa78c732410d050e2db04c
Vugar Dadalov <vuqar.ahmedoglu@gmail.com>
2024-01-04 22:16:44 +0400

    doc home page added card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,46 @@
-import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-
 import Heading from '@theme/Heading';
-import styles from './index.module.css';
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
+import Card from "@site/src/components/Card";
+import GridBox from "@site/src/components/GridBox";
+
+const cards = [
+  {
+    to: 'docs', title: 'Docs', description: 'Documentations'
+  },
+  {
+    to: 'docs/guides', title: 'Guides', description: 'Guides'
+  },
+  {
+    to: 'blog', title: 'Blog', description: 'Blog'
+  },
+  {
+    to: 'glossary', title: 'Glossary', description: 'Glossary'
+  }
+]
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={siteConfig.title}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+
+      <div className="container margin-top--lg padding-top--lg">
+        <Heading as="h3" className="hero__title">
+          IOMETE Resources
+        </Heading>
+        <p className="hero__subtitle">IOMETE Resources</p>
+
+        <article className="margin-top--lg">
+          <GridBox>
+            {cards.map(card => <Card key={card.to} title={card.title} link={card.to}>
+              {card.description}
+            </Card>)}
+          </GridBox>
+        </article>
+      </div>
     </Layout>
   );
 }
