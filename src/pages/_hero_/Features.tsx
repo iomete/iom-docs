e988256f3ddc631c5169298adb79d116cbbd5ac6
Nurlan Mammadov <36840386+nurlanmamedov@users.noreply.github.com>
2024-01-18 10:10:27 +0100

    Featured resources cards links fixed

--- src/pages/_hero_/Features.tsx
+++ src/pages/_hero_/Features.tsx
@@ -4,26 +4,26 @@ import styles from "./styles.module.scss";
 
 const cards = [
   {
-    to: "docs/community-deployment/overview",
+    to: "community-deployment/overview",
     title: "Community Deployment",
     description: "Learn how to deploy a community edition of IOMETE on Cloud or On-Premise.",
     label: "Getting Started",
   },
   {
-    to: "docs/user-guide/virtual-lakehouses",
+    to: "user-guide/virtual-lakehouses",
     title: "Virtual Lakehouses",
     description:
       "Learn how to create and manage virtual lakehouses which provides an SQL interface to query and process data in your data lakehouse.",
     label: "User Guide",
   },
   {
-    to: "docs/sql-quick-start/sql-ddl-examples",
+    to: "reference/sql-quick-start/sql-ddl-examples",
     title: "SQL Quickstart",
     description: "A quickstart guides to using SQL to query and process data in your data lakehouse.",
     label: "Reference",
   },
   {
-    to: "docs/guides/read-files-from-aws-s3",
+    to: "tutorials/read-files-from-aws-s3",
     title: "Loading Data",
     description: "Tutorials on how to query data files in S3 and from operational databases using JDBC providers.",
     label: "Tutorials",
