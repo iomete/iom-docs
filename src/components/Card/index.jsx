fe9214c9c1a45c61a4faa25539d47bdfbf281458
vusal-iom <vusal@iomete.com>
2024-01-26 04:00:51 +0100

    Support external links in Card component.
    This commit modifies how links are processed in the Card component. Initially, all links were combined with the base URL. Now, the approach checks if a link is external (starts with "http"). If true, the link remains unmodified; otherwise, it's appended to the base URL. This prevents incorrect routing for external links.

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,10 +4,12 @@ import "./style.scss";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 
 const Card = ({ title, icon, link, children }) => {
-  const baseUrl = useBaseUrl("/");
+
+  //if link is external, use link as is else use baseUrl + link
+  const url = link.startsWith("http") ? link : useBaseUrl("/") + link;
   return (
     <div className="iom-card">
-      <Link to={baseUrl + link}>
+      <Link to={url}>
         <div className="wrapper">
           {icon && <div className="icon-wrapper">{icon}</div>}
           <div className="content">
