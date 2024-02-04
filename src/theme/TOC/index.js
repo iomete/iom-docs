dcc34f5a87ae0750ab5fb6ce506b67d674aaada7
Nurlan Mammadov <36840386+nurlanmamedov@users.noreply.github.com>
2024-02-04 18:12:23 +0100

    Display the website on a large screen with a centralized layout, replacing the 'Contents' in the table of contents (TOC) with 'ON THIS PAGE

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -5,7 +5,7 @@ import styles from "./styles.module.scss";
 export default function TOCWrapper(props) {
   return (
     <div className={`toc-wrapper`}>
-      <h2>CONTENTS</h2>
+      <h2>ON THIS PAGE</h2>
       <TOC {...props} />
     </div>
   );
