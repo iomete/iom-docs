e9d22b4dc11bf898b3622ed8d99478016bd01294
Vugar Dadalov <vuqar.ahmedoglu@gmail.com>
2024-08-12 14:32:39 +0400

    Feedback component import

--- src/theme/DocItem/Footer/index.js
+++ src/theme/DocItem/Footer/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from '@theme-original/DocItem/Footer';
 import { useDoc } from '@docusaurus/plugin-content-docs/client';
+import Feedback from '@site/src/components/Feedback';
 export default function FooterWrapper(props) {
   const { metadata, frontMatter } = useDoc();
 
