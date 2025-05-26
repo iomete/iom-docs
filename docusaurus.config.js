f78dc76bc962f34ad7318f46bad9e7f53bc2fc7c
abhishekpathania <abhishek@iomete.com>
2025-05-26 16:41:30 +0530

    Update roadmap link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -207,7 +207,7 @@ const config = {
           },
 
           {
-            href: "https://github.com/orgs/iomete/projects/2",
+            href: "https://github.com/orgs/iomete/projects/7",
             position: "right",
             className: "link-icon github-link",
             "aria-label": "GitHub repository",
